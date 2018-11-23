1b7e56dfa12ee42cc4dba626320597333c978772
Andres Ardila <Arcandres@hotmail.com>
2018-11-23 10:05:59 -0500

    Main components

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+require('./assets/css/styles.css')
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header class="header">
+      <h1>Welcome to Blogger Reader</h1>
+    </header>
+    <section class="section">
+      <header>
+        <img src="/assets/img/logo.png" alt="Blogger Reader Logo">
+        <p>To begin with, first add a <b>Bloggers's</b> blog url.</p>
+        <BlogForm />
+      </header>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BlogForm from '@/components/BlogForm.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    BlogForm
   }
 }
 </script>
+
+<style scoped>
+.header, .section {
+  text-align: center;
+}
+
+.header {
+  background: #ddd;
+  border-bottom: 2px solid #bbb;
+  margin-bottom: 2rem;
+  padding: 2rem
+}
+
+.header h1{
+  color: #7E90B6;
+  margin: 0
+}
+</style>
+
