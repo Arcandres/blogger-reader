cd61af147d49286f670d39a140304f126554ca58
Andres Ardila <Arcandres@hotmail.com>
2018-11-23 10:35:59 -0500

    Post route

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,12 @@ const express = require('express'),
   app = express()
 
 app
-  .use(require('./get/blogs'))
-  .use(require('./get/blog'))
-  .use(require('./post/blog'))
+  // Get
+    .use(require('./get/blogs'))
+    .use(require('./get/blog'))
+    .use(require('./get/post'))
+    
+  // Post
+    .use(require('./post/blog'))
 
 module.exports = app
