24368b6b9177378162acaeeffacb9c376cfaa0a0
Andres Ardila <Arcandres@hotmail.com>
2018-11-23 10:19:16 -0500

    Get unique post

---
+++ routes/get/post.js
@@ -0,0 +1,18 @@
+const { googleApi, apiKey } = require('./../helpers/blogger'),
+  express = require('express'),
+  fetch = require('node-fetch'),
+  app = express()
+
+app.get('/blog/:blogId/:postId', (req, res) => {
+  // if (!data) {
+  //   throw new Error
+  // }
+
+  fetch(`${googleApi}/${req.params.blogId}/posts/${req.params.postId}?key=${apiKey}`)
+    // https://www.googleapis.com/blogger/v3/blogs/2399953/posts/7706273476706534553?key
+    .then(data => data.json())
+    .then(post => res.json(post))
+
+})
+
+module.exports = app
