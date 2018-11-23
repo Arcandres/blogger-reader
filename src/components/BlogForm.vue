681594a5ae99e13c87a65b3a66d14a6d1899fd2e
Andres Ardila <Arcandres@hotmail.com>
2018-11-23 10:04:15 -0500

    Main form

---
+++ src/components/BlogForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form id="BlogForm" class="blog-form">
+    <label for="BlogUrl"><b>URL</b></label> - 
+    <input id="BlogUrl" class="blog-url" placeholder="https://www.blog.com">
+    <p><button class="add-blog">+ Add a Blog</button></p>
+  </form>
+</template>
+
+<style>
+.blog-url {
+  background: #76859C;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  padding: .5rem;
+}
+
+.add-blog {
+  background: none;
+  border: 2px solid #818B95;
+  border-radius: 5px;
+  color: #76859C;
+  cursor: pointer;
+  font-weight: 600;
+
+  padding: .5rem
+}
+
+.add-blog:hover {
+  background: #76859C;
+  color: #fff
+}
+</style>
