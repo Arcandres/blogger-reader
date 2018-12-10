f60b2a489d5e7d20151b4ab41a03eb138c6b46a0
Arcandres <Arcandres@hotmail.com>
2018-12-10 01:56:17 -0500

    Fake DB

--- routes/get/blogs.js
+++ routes/get/blogs.js
@@ -2,7 +2,7 @@ const express = require('express'),
   app = express(),
   blogsDB = require('./../db/blogs.json')
 
-app.get('/blogs', (req, res) => {
+app.get('/', (req, res) => {
   res.json(blogsDB)
 })
 
