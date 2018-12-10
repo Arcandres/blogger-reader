d581c5cc11ef909e22e2db1836ab9203b51b0e70
Arcandres <Arcandres@hotmail.com>
2018-12-10 16:57:48 -0500

    Rendering blogs and its posts

---
+++ src/components/Blogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="blogs">
+    <ul>
+      <li v-for="blog in blogs">
+        <h3>{{ blog.name }}</h3>
+        <ul class="posts">
+          <li v-for="item in blog.items">
+            {{ item.title }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['blogs']
+}
+</script>
+
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Blogs v-bind:blogs="data" />
 </template>
+
+<script>
+import Blogs from '@/components/Blogs.vue'
+
+export default {
+  name: 'blogs',
+  components: {
+    Blogs
+  },
+  data() {
+    return {
+      data: [
+        {
+          "name": "Arcandres",
+          "items": [
+            {
+              "title": "Aplicaciones para cuidar y reducir el estrés ocular."
+            },
+            {
+              "title": "Wallpapers animados para tu escritorio."
+            },
+            {
+              "title": "Top 5 mejores fuentes para tu terminal"
+            }
+          ]
+        },
+        {
+          "name": "zkreations",
+          "items": [
+            {
+              "title": "Como alojar fuentes web sin pagar hosting"
+            },
+            {
+              "title": "Instala, optimizar y personalizar fuentes de Google fonts"
+            },
+            {
+              "title": "Reproductor de video html5 con Video.js"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  beforeCreate() {
+    // fetch('/blogs')
+    //   .then(data => data.json())
+    //   .then(blogs => this.data = blogs)
+    //   .catch(err => console.error(err))
+  }
+}
+</script>
