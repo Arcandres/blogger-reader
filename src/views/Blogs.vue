0eb8031496c79d13e846943bbf93f8517a6e44f3
Arcandres <Arcandres@hotmail.com>
2018-12-11 14:13:45 -0500

    Different id's

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -18,38 +18,38 @@ export default {
           "name": "Arcandres",
           "items": [
             {
-              "id": "17362163",
+              "id": "1736216",
               "title": "Aplicaciones para cuidar y reducir el estrés ocular.",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             },
             {
-              "id": "17362163",
+              "id": "1762163",
               "title": "Wallpapers animados para tu escritorio.",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             },
             {
-              "id": "17362163",
+              "id": "7362163",
               "title": "Top 5 mejores fuentes para tu terminal",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             }
           ]
         },
         {
-          "id": "4582943784",
+          "id": "4582943784a",
           "name": "zkreations",
           "items": [
             {
-              "id": "17362163",
+              "id": "1732163",
               "title": "Como alojar fuentes web sin pagar hosting",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             },
             {
-              "id": "17362163",
+              "id": "1762163",
               "title": "Instala, optimizar y personalizar fuentes de Google fonts",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             },
             {
-              "id": "17362163",
+              "id": "1736163",
               "title": "Reproductor de video html5 con Video.js",
               "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
             }
