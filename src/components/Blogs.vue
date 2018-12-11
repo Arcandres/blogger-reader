86c62e70e9497ffece1a9c62f1a2f715903cc5ed
Arcandres <Arcandres@hotmail.com>
2018-12-11 11:59:55 -0500

    Adding individual blog route

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -2,7 +2,7 @@
   <div id="blogs">
     <ul>
       <li :key="blog.id" v-for="blog in blogs">
-        <h3>{{ blog.name }}</h3>
+        <h3><router-link :to="'/blog/' + blog.id">{{ blog.name }}</router-link></h3>
         <ul class="posts">
           <li :key="item.id" v-for="item in blog.items">
             <router-link :to="'/blog/' + blog.id  + '/post/' + item.id">{{ item.title }}</router-link>
