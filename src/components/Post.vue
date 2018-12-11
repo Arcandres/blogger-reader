4618610aedd544e17b674c026349d15f2ac8e82a
Arcandres <Arcandres@hotmail.com>
2018-12-11 14:13:02 -0500

    Post structure

---
+++ src/components/Post.vue
@@ -0,0 +1,12 @@
+<template>
+  <section id="post">
+    <article v-html="post.content"></article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: ['post']
+}
+</script>
