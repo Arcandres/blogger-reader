529c3335f4b698911d04c77213d2debd9ce6c07e
Arcandres <Arcandres@hotmail.com>
2018-12-11 18:36:22 -0500

    Testing real data

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -13,56 +13,57 @@ export default {
   data() {
     return {
       data: [
-        {
-          "id": "4582943784",
-          "name": "Arcandres",
-          "items": [
-            {
-              "id": "1736216",
-              "title": "Aplicaciones para cuidar y reducir el estrés ocular.",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            },
-            {
-              "id": "1762163",
-              "title": "Wallpapers animados para tu escritorio.",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            },
-            {
-              "id": "7362163",
-              "title": "Top 5 mejores fuentes para tu terminal",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            }
-          ]
-        },
-        {
-          "id": "4582943784a",
-          "name": "zkreations",
-          "items": [
-            {
-              "id": "1732163",
-              "title": "Como alojar fuentes web sin pagar hosting",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            },
-            {
-              "id": "1762163",
-              "title": "Instala, optimizar y personalizar fuentes de Google fonts",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            },
-            {
-              "id": "1736163",
-              "title": "Reproductor de video html5 con Video.js",
-              "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
-            }
-          ]
-        }
+        // {
+        //   "id": "4582943784",
+        //   "name": "Arcandres",
+        //   "items": [
+        //     {
+        //       "id": "1736216",
+        //       "title": "Aplicaciones para cuidar y reducir el estrés ocular.",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     },
+        //     {
+        //       "id": "1762163",
+        //       "title": "Wallpapers animados para tu escritorio.",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     },
+        //     {
+        //       "id": "7362163",
+        //       "title": "Top 5 mejores fuentes para tu terminal",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     }
+        //   ]
+        // },
+        // {
+        //   "id": "4582943783",
+        //   "name": "zkreations",
+        //   "items": [
+        //     {
+        //       "id": "1732163",
+        //       "title": "Como alojar fuentes web sin pagar hosting",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     },
+        //     {
+        //       "id": "1762163",
+        //       "title": "Instala, optimizar y personalizar fuentes de Google fonts",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     },
+        //     {
+        //       "id": "1736163",
+        //       "title": "Reproductor de video html5 con Video.js",
+        //       "selfLink": "https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/8611549134915353489"
+        //     }
+        //   ]
+        // }
       ]
     }
   },
   beforeCreate() {
-    // fetch('/blogs')
-    //   .then(data => data.json())
-    //   .then(blogs => this.data = blogs)
-    //   .catch(err => console.error(err))
+    fetch('/blogs')
+      .then(blogs => {
+        console.log(blogs)
+        this.data.push(blogs)
+      })
   }
 }
 </script>
