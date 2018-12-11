04718d3cce406cdf2096d88265cf76f0aee6311b
Arcandres <Arcandres@hotmail.com>
2018-12-11 15:00:29 -0500

    Blog route added

--- src/config/router.js
+++ src/config/router.js
@@ -22,6 +22,14 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ '../views/Blogs.vue')
     },
     {
+      path: '/blog/:blodId',
+      name: 'blog',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../components/Blog.vue')
+    },
+    {
       path: '/blog/:blogId/post/:postId',
       name: 'post',
       component: () => import('../views/Post.vue')
