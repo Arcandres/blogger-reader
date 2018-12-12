8e2543d57559d920d9c79c53f321b4dd6b82d5fa
Arcandres <Arcandres@hotmail.com>
2018-12-12 00:49:56 -0500

    Rquest specific blog

--- routes/get/blogs.js
+++ routes/get/blogs.js
@@ -1,9 +1,14 @@
 const express = require('express'),
+  fetch = require('node-fetch'),
   app = express(),
+  { googleApi, apiKey } = require('../helpers/blogger'),
   blogsDB = require('./../db/blogs.json')
 
 app.get('/blogs', (req, res) => {
-  res.json(blogsDB)
+  fetch(`https://www.googleapis.com/blogger/v3/blogs/4964195606923028905/posts/?key=AIzaSyA3CsJnWRRu5YoPQqE9vMNKIed7cbOZJ3o`)
+    .then(data => data.json())
+    .then(blogs => res.send(blogs))
+    .catch(err => console.error(err))
 })
 
 module.exports = app
