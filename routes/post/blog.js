f6d88507bd73558a5979d80af2696e6f91a52a06
Arcandres <Arcandres@hotmail.com>
2018-12-13 17:40:25 -0500

    Using get instead of post

--- routes/post/blog.js
+++ routes/post/blog.js
@@ -5,10 +5,11 @@ const { googleApi, apiKey } = require('./../helpers/blogger'),
   app = express()
 
 app
-  .post('/blog', (req, res) => {
-    fetch(`${googleApi}/byurl?url=${/*req.body.url*/'https://www.arcandres.com'}&key=${apiKey}`)
+  .get('/blog', (req, res) => {
+    fetch(`${googleApi}/byurl?url=${req.query.url}&key=${apiKey}`)
       .then(data => data.json())
       .then(blog => fs.writeFile('blogs.json', JSON.stringify(blog), err => err ? console.log(err) : console.log('Blog saved...')))
+      .then(() => res.redirect('/'))
   })
 
 module.exports = app
