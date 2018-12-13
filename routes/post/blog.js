74a44fed93036f560150e0adcff8272513cf280c
Arcandres <Arcandres@hotmail.com>
2018-12-13 16:00:36 -0500

    Saving blog data locally

--- routes/post/blog.js
+++ routes/post/blog.js
@@ -1,13 +1,14 @@
 const { googleApi, apiKey } = require('./../helpers/blogger'),
-  express = require('express'),
   fetch = require('node-fetch'),
+  express = require('express'),
+  fs = require('fs'),
   app = express()
 
 app
   .post('/blog', (req, res) => {
-    fetch(`${googleApi}/byurl?url=${req.body.url}&key=${apiKey}`)
+    fetch(`${googleApi}/byurl?url=${/*req.body.url*/'https://www.arcandres.com'}&key=${apiKey}`)
       .then(data => data.json())
-      .then(blog => res.json(blog))
+      .then(blog => fs.writeFile('blogs.json', JSON.stringify(blog), err => err ? console.log(err) : console.log('Blog saved...')))
   })
 
 module.exports = app
