280f70581663bbaecf99d41f6d5735109c314154
Arcandres <Arcandres@hotmail.com>
2018-12-14 14:32:00 -0500

    Checking for existing data and appending new

--- routes/post/blog.js
+++ routes/post/blog.js
@@ -7,16 +7,34 @@ const { googleApi, apiKey } = require('./../helpers/blogger'),
 
 app
   .get('/addBlog', (req, res) => {
+    let blogs = []
+
     if (!fs.existsSync(temporalPath)) {
       return fetch(`${googleApi}/byurl?url=${req.query.url}&key=${apiKey}`)
         .then(data => data.json())
-        .then(blog => fs.writeFile(temporalPath, JSON.stringify(blog), err => err ? console.log(err) : console.log('Blog saved...')))
-        .then(() => res.redirect('/blog'))
+        .then(blog => {
+          blogs.push(blog)
+          fs.writeFile(temporalPath, JSON.stringify(blogs), err => err ? console.log(err) : console.log('Blog saved...'))
+        })
+        .then(() => res.redirect('/'))
     }
 
-    const blogs = require(temporalPath)
-    console.log(blogs.kind)
-    // return res.redirect('/')
+    blogs = require(temporalPath)
+    let l = blogs.length,
+      blogUrl = req.query.url.replace('https','http') + '/'
+    
+    while (l--) {
+      if (blogUrl === blogs[l].url) return
+    }
+
+    fetch(`${googleApi}/byurl?url=${req.query.url}&key=${apiKey}`)
+        .then(data => data.json())
+        .then(blog => {
+          blogs.push(blog)
+          fs.writeFile(temporalPath, JSON.stringify(blogs), err => err ? console.log(err) : console.log('Blog saved...'))
+        })
+
+    return res.redirect('/')
   })
 
 module.exports = app
