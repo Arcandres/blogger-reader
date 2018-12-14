51ba07215417df611ea003ca1c7da19601f8fda9
Arcandres <Arcandres@hotmail.com>
2018-12-13 23:47:19 -0500

    Checking for existing data

--- routes/post/blog.js
+++ routes/post/blog.js
@@ -2,14 +2,21 @@ const { googleApi, apiKey } = require('./../helpers/blogger'),
   fetch = require('node-fetch'),
   express = require('express'),
   fs = require('fs'),
+  temporalPath = '/home/arcandres/Documents/blogs.json',
   app = express()
 
 app
   .get('/blog', (req, res) => {
-    fetch(`${googleApi}/byurl?url=${req.query.url}&key=${apiKey}`)
-      .then(data => data.json())
-      .then(blog => fs.writeFile('blogs.json', JSON.stringify(blog), err => err ? console.log(err) : console.log('Blog saved...')))
-      .then(() => res.redirect('/'))
+    if (!fs.existsSync(temporalPath)) {
+      return fetch(`${googleApi}/byurl?url=${req.query.url}&key=${apiKey}`)
+        .then(data => data.json())
+        .then(blog => fs.writeFile(temporalPath, JSON.stringify(blog), err => err ? console.log(err) : console.log('Blog saved...')))
+        .then(() => res.redirect('/blog'))
+    }
+
+    const blogs = require(temporalPath)
+    console.log(blogs.kind)
+    return res.redirect('/')
   })
 
 module.exports = app
